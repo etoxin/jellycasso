561cc455414c7354fae38ffc2b458827a148ea5f
etoxin <adam@etoxin.net>
2014-03-25 12:59:12 +1100

    Added simple router

--- src/sugarkick.js
+++ src/sugarkick.js
@@ -18,4 +18,24 @@ sugarkick.template = function (name, template) {
     sugarkick.partials[name] = template;
 };
 
-sugarkick.router = function () {}
+sugarkick.router = function () {
+
+    //TODO: see if an app is defined in the dom
+
+    var view = document.getElementById('sugar-view');
+
+    //TODO: get the hashbang, strip the bang
+    var hashbang = window.location.hash.replace('#!','');
+
+    //TODO: match the hash with a route
+    if(hashbang && sugarkick.routes[hashbang]){
+        console.log('we have a matching route');
+        //TODO: render a template
+
+        view.innerHTML = sugarkick.partials[hashbang+'Template']
+    } else {
+        console.log('Hashbang does not match route');
+    }
+}
+window.addEventListener('hashchange', sugarkick.router);
+window.addEventListener('load', sugarkick.router);
